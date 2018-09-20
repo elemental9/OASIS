d885f4b78b691ddc6d3cae288af7dec7225bdd9b
Richie Bendall <richiebendall@gmail.com>
2018-09-20 18:22:15 +1200

    Update builder

--- ci/build.sh
+++ ci/build.sh
@@ -1,56 +1,42 @@
 #! /bin/sh
 
-# NOTE the command args below make the assumption that your Unity project folder is
-#  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test" 
-#  the project folder is "UnityProject". If this is not true then adjust the 
-#  -projectPath argument to point to the right location.
+project="<YOUR PROJECT NAME HERE>"
 
-## Run the editor unit tests
-echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml \
-	-quit
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity
+  -batchmode
+  -nographics
+  -silent-crashes
+  -logFile $(pwd)/unity.log
+  -projectPath $(pwd)
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe"
+  -quit
 
-rc0=$?
-echo "Unit test logs"
-cat $(pwd)/test.xml
-# exit if tests failed
-if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity
+  -batchmode
+  -nographics
+  -silent-crashes
+  -logFile $(pwd)/unity.log
+  -projectPath $(pwd)
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app"
+  -quit
 
-## Make the builds
-# Recall from install.sh that a separate module was needed for Windows build support
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
-	-quit
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity
+  -batchmode
+  -nographics
+  -silent-crashes
+  -logFile $(pwd)/unity.log
+  -projectPath $(pwd)
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe"
+  -quit
 
-rc1=$?
-echo "Build logs (Windows)"
+echo 'Logs from build'
 cat $(pwd)/unity.log
 
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-	-quit
 
-rc2=$?
-echo "Build logs (OSX)"
-cat $(pwd)/unity.log
-
-exit $(($rc1|$rc2))
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
--- ci/install.sh
+++ ci/install.sh
@@ -1,21 +1,29 @@
 #! /bin/sh
 
-# Download Unity3D installer into the container
-#  The below link will need to change depending on the version, this one is for 2018.2.8f1 (originally 5.5.1)
-#  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
-echo 'Downloading Unity 2018.2.8f1 pkg:'
-curl --retry 5 -o Unity.pkg https://netstorage.unity3d.com/unity/ae1180820377/MacEditorInstaller/Unity-2018.2.8f1.pkg
-if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+BASE_URL=https://netstorage.unity3d.com/unity
+HASH=88d00a7498cd
+VERSION=5.5.1f1
 
-# In Unity 5 they split up build platform support into modules which are installed separately
-# By default, only Mac OSX support is included in the original editor package; Windows, Linux, iOS, Android, and others are separate
-# In this example we download Windows support. Refer to http://unity.grimdork.net/ to see what form the URLs should take
-echo 'Downloading Unity 2017.1.2p4 Windows Build Support pkg:'
-curl --retry 5 -o Unity_win.pkg http://download.unity3d.com/download_unity/d597d0924185/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.1.2p4.pkg
-if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
 
-# Run installer(s)
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
-echo 'Installing Unity_win.pkg'
-sudo installer -dumplog -package Unity_win.pkg -target /
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
