d4a6d8c8853a29040aa8689150551ca70ba771fc
Richie Bendall <richiebendall@gmail.com>
2018-09-27 23:09:32 +1200

    Update dir specification

--- ci/build.sh
+++ ci/build.sh
@@ -3,13 +3,13 @@
 project="primary_code"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/$(project)" -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" -quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/$(project)" -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" -quit
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath $(pwd) -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "$(pwd)/$(project)" -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
