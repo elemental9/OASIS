1aa94b713224c37c960bc9b4131777f2460edec5
Richie Bendall <richiebendall@gmail.com>
2018-10-03 20:15:02 +1300

    Updated files

--- OASIS/Assets/Editor/build.cs
+++ OASIS/Assets/Editor/build.cs
@@ -1,12 +1,8 @@
 using UnityEditor;
-class WebGLBuilder {
-    static void build() {
 
-        // Place all your scenes here
-        string[] scenes = {"Assets/scenes/SampleScene.unity"};
-
-        string pathToDeploy = "builds/WebGLversion/";       
-
-        BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);      
-    }
-}
+  class WebGLBuilder {
+      static void build() {
+          string[] scenes = {"Assets/Scenes/Main.unity"};
+          BuildPipeline.BuildPlayer(scenes, "dist", BuildTarget.WebGL, BuildOptions.None);
+      }
+  }
