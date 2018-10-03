7f54cfdb0f7ac048665e6cfb9c5c0430534d260e
Richie Bendall <richiebendall@gmail.com>
2018-10-03 21:07:10 +1300

    Use a debian package instead

--- ci/install-unity.sh
+++ ci/install-unity.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Does the Unity installer already exist (from cache)?
-if [ -e "./cache/unity-installer.sh" ]; then
+if [ -e "./cache/unity-installer.deb" ]; then
     echo 'File already exists. Download not required.'
 else
     # Download the Unity installer
-    curl -L -o cache/unity-installer.sh http://beta.unity3d.com/download/fd37f3680b5f/unity-editor-installer-2017.2.0b11.sh
+    curl -L -o ./cache/unity-installer.deb http://beta.unity3d.com/download/fd37f3680b5f/unity-editor_amd64-2017.2.0b11.deb
 fi
 
 # Make Unity installer executable
-chmod +x ./cache/unity-installer.sh
+chmod +x ./cache/unity-installer.deb
 
 # Run the Unity installer
-sudo ./cache/unity-installer.sh
+sudo dpkg -i unity-installer.deb
